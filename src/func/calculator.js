7421f80ae9ad08bb59f5e1c0c25b4b7601325010
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-10-07 11:10:29 +0800

    Add calculator functionality and update HTML structure

--- src/func/calculator.js
+++ src/func/calculator.js
@@ -8,3 +8,21 @@
  * @returns {num} txtareaComputed
  * 
  */
+
+const getInputFirstNumber = (num) => {
+    const FIRST_NUMBER = document.getElementById("firsNumber");
+    const value = FIRST_NUMBER.value.trim().innerHTML = num ;
+    
+    return value ;
+}
+
+
+// Main 
+
+function calculateHandler() {
+
+    const firstNumber = getInputFirstNumber() ;
+    
+}
+
+export default calculateHandler ;
