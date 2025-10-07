4abd3d9c23e04100820aa372edad6105ed47c573
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-10-07 10:32:37 +0800

    Updated

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import './style.css'
 
+// Explanation 
+// Create a simple calculator using the operators 
+
 document.querySelector('#app').innerHTML = `
   <div>
-    
+    <form>
+      <fieldset>
+        <legend>Calculator</legend>
+        <input type="text" id="firstNumber" name="inputNumber"><br>
+        <p><!--This is empty for displaying only--></p>
+        <input type="text" id="secondNumber" name="inputNumber"><br><br>
+        <input type="submit" value="Calculate" id="inptCalculate">
+      </fieldset>
+    </form>
   </div>
 `
